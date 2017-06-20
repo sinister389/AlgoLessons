25985145b95b34a918b41c5b28ae1f30242a9c11
sinister389 <modern45rega@gmail.com>
2017-06-21 02:05:45 +0700

    Revert "fix for 1_1_19 + new 1_1_20 and 1_1_22"
    This reverts commit 6778a78c5fed3bcc1e534d20e3acc41b18d6cce1.

--- src/Ex_1_1_19/Fibonacci.java
+++ src/Ex_1_1_19/Fibonacci.java
@@ -3,19 +3,19 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Fibonacci {
     public static long Fib(int N) {
+        long[] F = new long[N + 1];
         if (N < 0) {
             StdOut.println("Error: N < 0");
             return -1;
         }
         if (N == 0) return 0;
         if (N == 1) return 1;
-        long[] F = new long[N + 1];
         F[0] = 0;
         F[1] = 1;
         for (int i = 2; i <= N; i++) {
             F[i] = F[i - 1] + F[i - 2];
         }
-        return F[N];
+        return F[F.length - 1];
     }
 
     public static void main(String[] args) {
